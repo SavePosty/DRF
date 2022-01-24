2903637c324c0659de6db29dee574e72ab688085
Prabhatrajput7 <prabhat.rajput@gmail.com>
2022-01-24 10:44:09 +0530

    hello

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,22 +3,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
-
+# u need these 2 AbstractBaseUser, PermissionsMixin to overide default django admin
+# default manager for django BaseUserManager
 # Create your models here.
 
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profile"""
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, name, password=None): #these are default fx that django cli uses
         """Create a new user profile"""
         if not email:
             raise ValueError('user must have a email address')
 
-        email = self.normalize_email(email)#second half of email all lower case
-        user = self.model(email=email,name=name) #new model object
+        email = self.normalize_email(email)#make email second half of email all lower case #first half is case sensitive
+        user = self.model(email=email,name=name) #new model object srt the email and name
 
-        user.set_password(password) #AbstractBaseUser fx haching password
-        user.save(using=self._db)
+        user.set_password(password) #AbstractBaseUser fx haching password default fx
+        user.save(using=self._db) # default saving object djanfo
 
         return user
 
@@ -35,15 +36,15 @@ class UserProfileManager(BaseUserManager):
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database Model for users in the System"""
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)# unique email col
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)#checking user is active or not
+    is_active = models.BooleanField(default=True)#checking user profile is activated or not or not
     is_staff = models.BooleanField(default=False) #check if user is a staff user or not i.e right to access Django Admin
 
     objects = UserProfileManager()#custom module manager for the objects to use with django CLI
     
     USERNAME_FIELD = 'email' #overrifing deafult username filed with email
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['name'] #additional required field
 
     def get_full_name(self):
         """Get Full Name"""
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloAppiView(APIView):
+    """Test API VIEW"""
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Users HTTP methods as fx (get, post, patch, put delete)',
+            'Is similar to default dj view',
+            'gives you the comst control of application logic',
+            'is maaped manually to urls',
+        ]
+
+        return Response({'message':'Hello There',
+        'an_apiview':an_apiview}) #when api is called it must retuen a response
+
